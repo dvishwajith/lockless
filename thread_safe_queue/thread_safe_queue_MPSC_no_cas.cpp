74407433086d2dbfb2695209998231960e4e83d8
dvishwajith <dvishwajith@gmail.com>
2025-03-04 22:40:42 -0800

    MPSC no cas

--- thread_safe_queue/thread_safe_queue_MPSC_no_cas.cpp
+++ thread_safe_queue/thread_safe_queue_MPSC_no_cas.cpp
@@ -34,12 +34,11 @@ bool dequeue(int &value) {
   return true;
 }
 
-#define TEST_SIZE 20000000
-#define NUMBER_OF_PRODUCERS 4
+#define TEST_SIZE 5
+#define NUMBER_OF_PRODUCERS 2
 
 bool test_mpsc_queue() {
   bool passed = true;
-  std::vector<int> test_values;
   std::vector<std::thread> producers;
   for (size_t i = 0; i < NUMBER_OF_PRODUCERS; i++)
   {
@@ -48,7 +47,6 @@ bool test_mpsc_queue() {
         while(!enqueue(i)) {
           std::this_thread::yield();
         }
-        test_values.push_back(i);
       }
       
     });    
@@ -56,34 +54,35 @@ bool test_mpsc_queue() {
   
 
 
-  std::vector<int> results;
+  std::unordered_map<int, int> results;
   std::thread consumer_thread([&] {
     for (size_t i = 0; i < TEST_SIZE; i++) {
       int value = 123456789;
       while(!dequeue(value)) {
         std::this_thread::yield();
       }
-      results.push_back(value);
+      auto it = results.find(value);
+      if (it == results.end()) {
+        results[value] = 1;
+      } else {
+        results[value]++;
+      }
     }
   });
-  
-  producer_thread.join();
+
+  for (auto &p : producers) {
+    p.join();
+  }
   consumer_thread.join();
-  if (test_values == results) {
-    std::cout << "Success\n";
-  } else {
-    passed = false;
-    std::cout << "Input and output vector mismatch\n";
-    std::cout << "inputs:\n";
-    if (test_values.size() < 50) {
-      for (auto &d : test_values) {
-        std::cout << d << " ";
-      }
-      std::cout << "\nresults:\n";
-      for (auto &d : results) {
-        std::cout << d << " ";
-      }
+  for (size_t i = 0; i < TEST_SIZE; i++) {
+    if (results[i] != NUMBER_OF_PRODUCERS) {
+      passed = false;
+      std::cout << "Failed: test value " << i << " count " << results[i] << "\n";
+      break;
     }
+  }
+  if (passed) {
+    std::cout << "Success\n";
   }
   return passed;
 }
@@ -93,13 +92,13 @@ int main() {
   auto end_time = start_time + std::chrono::hours(1);
 
   int test_count = 0;
-  while (std::chrono::steady_clock::now() < end_time) {
+  // while (std::chrono::steady_clock::now() < end_time) {
       if (!test_mpsc_queue()) {
-        std::cout << "test " << test_count << "failed\n";
+        std::cout << "test " << test_count << " failed\n";
         return -1;
       }
       test_count++;
-  }
+  // }
 
   std::cout << "Completed " << test_count << " tests in 1 hour." << std::endl;
   return 0;
