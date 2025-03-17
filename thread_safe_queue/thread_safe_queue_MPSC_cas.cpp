9604f1c46492b6b9ac71738d5df7adbc7b190010
dvishwajith <dvishwajith@gmail.com>
2025-03-16 20:30:10 -0700

    temp debug

--- thread_safe_queue/thread_safe_queue_MPSC_cas.cpp
+++ thread_safe_queue/thread_safe_queue_MPSC_cas.cpp
@@ -3,6 +3,16 @@
 #include <vector>
 #include <thread>
 
+// // incase we need to test all threads on same core
+// #include <mach/mach.h>
+// void set_thread_affinity(int tag) {
+//     thread_port_t thread = pthread_mach_thread_np(pthread_self());
+//     thread_affinity_policy_data_t policy = {tag};  // Set affinity tag
+//     thread_policy_set(thread, THREAD_AFFINITY_POLICY, (thread_policy_t)&policy, 1);
+// }
+
+
+
 #define QUEUE_SIZE 10
 
 std::atomic<u_int> head {0};
@@ -10,12 +20,12 @@ std::atomic<u_int> tail {0};
 
 struct Node
 {
-  std::atomic<int> ready; // 0 = Empty, 1 = full
+  std::atomic<bool> ready; // 0 = Empty, 1 = full
   int data;
 };
 
 
-Node data[QUEUE_SIZE];
+Node data[QUEUE_SIZE] = {};
 
 bool enqueue(int value) {
   // compare the tail + 1 instead tail
@@ -23,48 +33,68 @@ bool enqueue(int value) {
   // and notify QUEUE is full before it is actually full.
   // This allow as to use head == tail as empty condition
   // and tail + 1 == head as full condition.
-  u_int current_tail, next_tail;
+  u_int current_tail = 0, next_tail = 0;
   do {
-    current_tail = tail.load(std::memory_order_relaxed);
+    current_tail = tail.load(std::memory_order_seq_cst);
     next_tail = (current_tail + 1) % QUEUE_SIZE;
-    if (next_tail == head.load(std::memory_order_acquire)) {
+    std::atomic_thread_fence(std::memory_order_seq_cst); // Full memory barrier
+    if (next_tail == head.load(std::memory_order_seq_cst)) {
       return false;  // queue is full
     }
-  } while (!tail.compare_exchange_weak(current_tail, next_tail, std::memory_order_acq_rel));
-  data[next_tail].data = value;
-  data[next_tail].ready.store(1, std::memory_order_release);
+  } while (!tail.compare_exchange_weak(current_tail, next_tail, std::memory_order_seq_cst, std::memory_order_seq_cst));
+  data[current_tail].data = value;
+  bool ready = false;
+  std::atomic_thread_fence(std::memory_order_seq_cst); // Full memory barrier
+  data[current_tail].ready.compare_exchange_strong(ready, true, std::memory_order_seq_cst);
+  // data[current_tail].ready.store(1, std::memory_order_release);
   return true;
 }
 
-bool dequeue(int &value) {
-  u_int currentHead = head.load(std::memory_order_relaxed) % QUEUE_SIZE;
-  if (currentHead == tail.load(std::memory_order_acquire)) {
-    return false;  // queue is empty
+int dequeue(int &value) {
+  u_int currentHead = head.load(std::memory_order_seq_cst);
+  std::atomic_thread_fence(std::memory_order_seq_cst); // Full memory barrier
+  u_int currentTail = tail.load(std::memory_order_seq_cst);
+  if (currentHead == currentTail) {
+    return -1;  // queue is empty
   }
-  // if (!data[currentHead].ready.load(std::memory_order_acquire)) {
-  //   return false;
-  // }
+  if (!data[currentHead].ready.load(std::memory_order_seq_cst)) {
+    return -2;
+  }
+  std::atomic_thread_fence(std::memory_order_seq_cst); // Full memory barrier
   value = data[currentHead].data;
-  data[currentHead].ready.store(0, std::memory_order_release);
-  head.store((currentHead + 1) % QUEUE_SIZE, std::memory_order_release);
-  return true;
+
+  bool ready = true;
+  std::atomic_thread_fence(std::memory_order_seq_cst); // Full memory barrier
+  data[currentHead].ready.compare_exchange_strong(ready, false, std::memory_order_seq_cst);
+
+  u_int next_head = (currentHead + 1) % QUEUE_SIZE;
+  std::atomic_thread_fence(std::memory_order_seq_cst); // Full memory barrier
+  // data[currentHead].ready.store(0, std::memory_order_release);
+  head.store(next_head, std::memory_order_seq_cst);
+  return 0;
 }
 
-#define TEST_SIZE 20
-#define NUMBER_OF_PRODUCERS 4
+#define TEST_SIZE 2000
+#define NUMBER_OF_PRODUCERS 6
 
 bool test_mpsc_queue() {
   bool passed = true;
   std::vector<std::thread> producers;
-  for (size_t i = 0; i < NUMBER_OF_PRODUCERS; i++)
+  for (size_t p = 0; p < NUMBER_OF_PRODUCERS; p++)
   {
     producers.emplace_back([&] () {
-      for (size_t i = 1; i < TEST_SIZE; i++) {
+      for (size_t i = 0; i < TEST_SIZE; i++) {
+        auto start_time = std::chrono::steady_clock::now();
+        auto end_time = start_time + std::chrono::seconds(1);
         while(!enqueue(i)) {
-          std::this_thread::yield();
+          // std::this_thread::yield();
+          std::this_thread::sleep_for(std::chrono::nanoseconds(5));
+          if (std::chrono::steady_clock::now() > end_time) {
+            std::cout << "producer wait exceeded when writing index " << i << " true index " << p << "\n";
+            break;
+          }
         }
       }
-      
     });    
   }
   
@@ -72,11 +102,32 @@ bool test_mpsc_queue() {
 
   std::unordered_map<int, int> results;
   std::thread consumer_thread([&] {
-    for (size_t i = 1; i < TEST_SIZE - 1; i++) {
+    auto start_time = std::chrono::steady_clock::now();
+    auto end_time = start_time + std::chrono::seconds(5);
+    for (size_t i = 0; i < TEST_SIZE*NUMBER_OF_PRODUCERS; i++) {
       int value = 123456789;
-      while(!dequeue(value)) {
-        std::this_thread::yield();
+      int ret = -10;
+      uint empty_count = 0;
+      uint not_ready_count = 0;
+      ret = dequeue(value);
+      while(ret != 0) {
+        if (ret == -1) {
+          empty_count++;
+        } else if (ret == -2) {
+          not_ready_count++;
+        }
+        if (std::chrono::steady_clock::now() > end_time) {
+          std::cout << "consumer wait exceeded when reading index " << i << " true index " << i/NUMBER_OF_PRODUCERS << " empty_count " << empty_count << " not_ready_count " << not_ready_count << "\n";
+          break;
+        }
+        // std::this_thread::yield();
+        std::this_thread::sleep_for(std::chrono::nanoseconds(5));
+        ret = dequeue(value);
       }
+      if (std::chrono::steady_clock::now() > end_time) {
+        break;
+      }
+      // std::cout << "dequeu val " << value << "\n";
       auto it = results.find(value);
       if (it == results.end()) {
         results[value] = 1;
@@ -86,15 +137,18 @@ bool test_mpsc_queue() {
     }
   });
 
+  consumer_thread.join();
   for (auto &p : producers) {
     p.join();
   }
-  consumer_thread.join();
-  for (size_t i = 1; i < TEST_SIZE - 1; i++) {
+  
+  for (size_t i = 0; i < TEST_SIZE ; i++) {
     if (results[i] != NUMBER_OF_PRODUCERS) {
       passed = false;
-      std::cout << "Failed: test value " << i << " count " << results[i] << "\n";
+      std::cout << "Failed: test value " << i << " count " << results[i] << "size of result vector" << results.size() << "\n";
       break;
+    } else {
+      // std::cout << "        test value " << i << " count " << results[i] << "\n";
     }
   }
   if (passed) {
@@ -105,16 +159,16 @@ bool test_mpsc_queue() {
 
 int main() {
   auto start_time = std::chrono::steady_clock::now();
-  auto end_time = start_time + std::chrono::hours(1);
+  auto end_time = start_time + std::chrono::minutes(10);
 
   int test_count = 0;
-  // while (std::chrono::steady_clock::now() < end_time) {
+  while (std::chrono::steady_clock::now() < end_time) {
       if (!test_mpsc_queue()) {
         std::cout << "test " << test_count << " failed\n";
         return -1;
       }
       test_count++;
-  // }
+  }
 
   std::cout << "Completed " << test_count << " tests in 1 hour." << std::endl;
   return 0;
